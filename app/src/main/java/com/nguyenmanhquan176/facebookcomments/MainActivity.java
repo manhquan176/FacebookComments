1f76ba6bea2c088249473ed1845e25213e1dc00b
framgia <framgia@framgia0138-pc.framgia.com>
2016-09-13 13:38:58 +0700

    first commit

---
+++ app/src/main/java/com/nguyenmanhquan176/facebookcomments/MainActivity.java
@@ -0,0 +1,65 @@
+package com.nguyenmanhquan176.facebookcomments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static String url = "http://api.androidhive.info/facebook/firebase_analytics.html";
+
+  private WebView webView;
+
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    webView = (WebView) findViewById(R.id.web_view);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // launching facebook comments activity
+        Intent intent = new Intent(MainActivity.this, FbCommentsActivity.class);
+
+        // passing the article url
+        intent.putExtra("url", "http://www.androidhive.info/2016/06/android-firebase-integrate-analytics/");
+        startActivity(intent);
+      }
+    });
+
+    // loading web page
+    webView.loadUrl(url);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
